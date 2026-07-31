d506c2bac6ad147a1a240b7858f455374550714e
Macbook <macbook@MacBook-Pro.local>
2026-07-31 23:21:21 +0700

    docs: added analysis and user's data

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import {registerServiceWorker} from './lib/pwa';
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
 
